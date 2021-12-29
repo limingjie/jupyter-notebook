96e5a5a579cc36e2b610c84604d9f6566a91e037
mingjie.li <mingjie.li@everbridge.com>
2021-12-29 17:08:33 +0800

    Update jupyter notebook to PDF guide and script

--- Guides/convert-jupyter-notebook-with-cjk-to-pdf-on-macos.md
+++ Guides/convert-jupyter-notebook-with-cjk-to-pdf-on-macos.md
@@ -12,7 +12,7 @@ brew install mactex
 
 ## Install CJK Font
 
-Use system font or install any CJK font. I prefer [Sarasa Gothic (更纱黑体 / 更紗黑體 / 更紗ゴシック / 사라사고딕)](https://github.com/be5invis/Sarasa-Gothic).
+Use system font or install any CJK font. I recommend [Sarasa Gothic (更纱黑体 / 更紗黑體 / 更紗ゴシック / 사라사고딕)](https://github.com/be5invis/Sarasa-Gothic).
 
 ```shell
 brew install font-sarasa-gothic
@@ -43,3 +43,37 @@ brew install font-sarasa-gothic
     ```shell
     xelatex notebook-to-convert.tex > notebook-to-convert.pdf
     ```
+
+## Shell Script Helper
+
+```shell
+#!/bin/zsh
+
+FILE="notebook-to-convert"
+
+# Remove files
+rm $FILE.html $FILE.tex $FILE.md $FILE.pdf
+
+# Generate HTML
+jupyter nbconvert --to html $FILE.ipynb
+# Install latest nbconvert to get line number option https://github.com/jupyter/nbconvert
+# jupyter nbconvert --to html $FILE.ipynb --Highlight2HTML.extra_formatter_options linenos=table
+
+# Generate Markdown
+jupyter nbconvert --to markdown $FILE.ipynb
+
+# Generate Tex
+jupyter nbconvert --to latex $FILE.ipynb
+
+# Enable CJK
+# Note: sed MacOS syntax
+sed -i '' '2i\
+    \\usepackage[slantfont, boldfont]{xeCJK}\
+    \\setCJKmainfont{Sarasa UI SC}' $FILE.tex
+
+# Generate PDF
+xelatex $FILE.tex
+
+# cleanup
+rm $FILE.log $FILE.out $FILE.aux
+```
--- WanTing/build.sh
+++ WanTing/build.sh
@@ -3,32 +3,34 @@
 # Follow the guide to install dependencies and fonts
 # https://limingjie.github.io/posts/2021-12-28-convert-jupyter-notebook-with-cjk-to-pdf-on-macos/
 
+FILE="python"
+
 # Remove files
-rm python.html python.tex python.md python.pdf
+rm $FILE.html $FILE.tex $FILE.md $FILE.pdf
 
 # Generate HTML
-jupyter nbconvert --to html python.ipynb
+jupyter nbconvert --to html $FILE.ipynb
 # Install latest nbconvert to get line number option https://github.com/jupyter/nbconvert
-# jupyter nbconvert --to html python.ipynb --Highlight2HTML.extra_formatter_options linenos=table
+# jupyter nbconvert --to html $FILE.ipynb --Highlight2HTML.extra_formatter_options linenos=table
 
 # Generate Markdown
-jupyter nbconvert --to markdown python.ipynb
+jupyter nbconvert --to markdown $FILE.ipynb
 
 # Generate Tex
-jupyter nbconvert --to latex python.ipynb
+jupyter nbconvert --to latex $FILE.ipynb
 
 # Enable CJK
 # Note: sed MacOS syntax
 sed -i '' '2i\
     \\usepackage[slantfont, boldfont]{xeCJK}\
-    \\setCJKmainfont{Sarasa UI SC}' python.tex
+    \\setCJKmainfont{Sarasa UI SC}' $FILE.tex
 
 # Change PDF title
 # Note: sed MacOS syntax
-sed -i '' 's/\\title{python}/\\title{黄师姐 Python 秘籍}/g' python.tex
+sed -i '' 's/\\title{python}/\\title{黄师姐 Python 秘籍}/g' $FILE.tex
 
 # Generate PDF
-xelatex python.tex
+xelatex $FILE.tex
 
 # cleanup
-rm python.log python.out python.aux
+rm $FILE.log $FILE.out $FILE.aux
